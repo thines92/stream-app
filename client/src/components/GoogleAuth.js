b22159f67670732f7a30fd9f976ef6dff5cb4d3f
Tyler <tylerhines1992@gmail.com>
2020-05-28 19:59:15 -0400

    Add isSignedIn state for gAPI

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,33 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+	state = { isSignedIn: null };
+
 	componentDidMount() {
 		window.gapi.load("client:auth2", () => {
 			window.gapi.client.init({
 				clientId:
-					"81166866981-agb6v4ou05d5rmgabmi6lddpj1eadk0b.apps.googleusercontent.com",
+					"838810902099-lfivbok5pb3vnht6rqre4fgdt2tlunh0.apps.googleusercontent.com",
 				scope: "email"
-			});
+			}).then(() => {
+				this.auth = window.gapi.auth2.getAuthInstance();
+				this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+			})
 		});
 	}
 
+	renderAuthButton() {
+		if (this.state.isSignedIn === null) {
+			return <div>I don't know if we are signed in</div>
+		} else if (this.state.isSignedIn) {
+			return <div>Signed In</div>
+		} else {
+			return <div>I am not signed in</div>
+		}
+	}
+
 	render() {
-		return <div>Google Auth</div>;
+		return <div>{this.renderAuthButton()}</div>;
 	}
 }
 
