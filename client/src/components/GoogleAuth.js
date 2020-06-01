4e613a46e3f225c2f78b25a255247d416cb8ea59
Tyler <tylerhines1992@gmail.com>
2020-06-01 19:22:45 -0400

    add google ID as parameter in action call

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -20,9 +20,9 @@ class GoogleAuth extends React.Component {
 
 	onAuthChange = (isSignedIn) => {
 		if (isSignedIn) {
-			this.props.signIn();
+			this.props.signIn(this.auth.currentUser.get().getId());
 		} else {
-			this.props.signOut();
+			this.props.signOut(this.auth.currentUser.get().getId());
 		}
 	}
 
