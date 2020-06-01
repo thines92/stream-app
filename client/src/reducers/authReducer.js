0b8dc30fc7a59f47ed5221a5ddb99d461ecb9126
Tyler <tylerhines1992@gmail.com>
2020-06-01 19:22:01 -0400

    add default on switch

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -10,7 +10,7 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, isSignedIn: true }
         case SIGN_OUT:
             return { ...state, isSignedIn: false }
+        default:
+            return state
     }
-
-    return state;
 }
